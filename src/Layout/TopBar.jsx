b966f17eda39bae94e54ca6f6a72728d67787dd8
Varsha Kadge <varshakadge@Varshas-Air.lan>
2026-03-11 22:46:23 +0530

    API Integration

--- src/Layout/TopBar.jsx
+++ src/Layout/TopBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 
 function Topbar({ onCatClick, searchTerm, setSearchTerm }) {
   const [categories, setCategories] = useState([]);
